db56c5008fe9304daa86631c88241911ff40eab4
HAPDeNovo <xzhou15@stanford.edu>
2017-01-19 18:24:46 -0800

    Update run_denovo_step2.sh

--- src/run_denovo_step2.sh
+++ src/run_denovo_step2.sh
@@ -7,7 +7,7 @@ chr_end=${10}
 output_prefix=${12}
 
 ##### Step 2: longranger variant call for single vcf to phase vcf, then merge them to one vcf
-echo "running Step 3..."
+echo "running Step 2..."
 echo "chr_start="$chr_start" , chr_end="$chr_end
 ./cut_chr_from_phased_vcf.sh $child_vcfgz $parent1_vcfgz $parent2_vcfgz $chr_start $chr_end $output_prefix
 
