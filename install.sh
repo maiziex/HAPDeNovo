c032249bb50efdef21df548bb58e90bb591ccc0b
Xin Zhou <maiziex@gmail.com>
2017-08-22 23:05:40 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 wget http://xinzhouneuroscience.org/wp-content/uploads/2017/08/lib.tar.gz
-tar -zxvf lib.tar.gz
 wget http://xinzhouneuroscience.org/wp-content/uploads/2017/08/example.tar.gz
+tar -zxvf lib.tar.gz
 tar -zxvf example.tar.gz
 chmod +x src/*.sh
 chmod +x lib/GenomeAnalysisTK/GenomeAnalysisTK.jar
