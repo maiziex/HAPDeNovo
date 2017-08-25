f091000e1d784b1b937040e110f36091bb7c7f0b
Xin Zhou <maiziex@gmail.com>
2017-08-24 23:45:56 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@ wget http://xinzhouneuroscience.org/wp-content/uploads/2017/08/example.tar.gz
 wget http://xinzhouneuroscience.org/wp-content/uploads/2017/08/lib.tar.gz
 tar -zxvf lib.tar.gz
 tar -zxvf example.tar.gz
+cd lib
+tar -xzvf longranger-2.1.5.tar.gz
+mv longranger-2.1.5 longranger
+cd ..
 chmod +x src/*.sh
 chmod +x lib/GenomeAnalysisTK/GenomeAnalysisTK.jar
 rm lib.tar.gz
