80486cac584b99228254bf3669e844f311bd4c9a
Xin Zhou <maiziex@gmail.com>
2018-05-02 17:42:10 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 wget http://xinzhouneuroscience.org/wp-content/uploads/2017/08/example.tar.gz
-wget http://xinzhouneuroscience.org/wp-content/uploads/2017/08/lib.tar.gz
+wget http://xinzhouneuroscience.org/wp-content/uploads/2018/05/lib.tar.gz
 tar -zxvf lib.tar.gz
 tar -zxvf example.tar.gz
 cd lib
